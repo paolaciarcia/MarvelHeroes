4f55aa12288a64c3404975bf768763e6fd2f2654
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-07-02 12:17:22 -0300

    add HeroesTableViewController, HeroViewController and HeroTableViewCell

---
+++ MarvelHeroes/HeroTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeroTableViewCell.swift
+//  MarvelHeroes
+//
+//  Created by Erick Borges on 02/07/21.
+//
+
+import UIKit
+
+class HeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
