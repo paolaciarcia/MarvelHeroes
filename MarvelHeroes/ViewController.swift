c5921515b6ced2ecd381f3b6a8f414293838fb52
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-07-02 11:43:20 -0300

    add layout to ViewController

--- MarvelHeroes/ViewController.swift
+++ MarvelHeroes/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundView.layer.borderWidth = 3
+        backgroundView.layer.borderColor = UIColor.black.cgColor
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
 }
 
